23829768bc50c7acae902ea38d8f0706e4811d6c
Jessie Chen <jc3496@princeton.edu>
2025-01-07 21:47:02 -0800

    more css changes!

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <p className={styles.paragraph}>I am an undergraduate student at Princeton University studying Computer Science and minoring in Statistics and Machine Learning as well as Finance.</p>
         <p className={styles.paragraph}>I am passionate about leveraging technology for societal needs as well as using technology for fun experiences such as digital birthday cards for long distance friends and family!</p>
         <p className={styles.paragraph}>Previously, I was an Innovation Developer Intern at Blue Shield of California where I contributed to the automation of prior authorization using Da Vinci and the development of an online consolidation of member health data called Member Health Record (MHR).</p>
-        <p className={styles.paragraph}>When I am not developing, I love watching older movies and TV shows (so nostalgic!), making Spotify playlists for friends, karaoke-ing, eating lots of yummy food, and indulging in my, very long, TBR list! Let's connect!</p>
+        <p className={styles.paragraph}>When I am not developing, I love watching older movies and TV shows (so nostalgic!), making Spotify playlists for friends, karaokeing, eating lots of yummy food, and indulging in my, very long, TBR list! Let's connect!</p>
         <a href="/resume" className="btn">View Resume</a>
       </main>
     </>
