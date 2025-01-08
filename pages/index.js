798a5cd2120c63fc49a21ac6ad50287d8d3b2f81
Jessie Chen <jc3496@princeton.edu>
2025-01-07 21:43:26 -0800

    css changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Navbar from '../components/Navbar';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -8,12 +9,12 @@ export default function Home() {
         <title>About Me</title>
       </Head>
       <Navbar />
-      <main>
-        <h1>Hi, I'm Jessie!</h1>
-        <p>I am an undergraduate student at Princeton University studying Computer Science and minoring in Statistics and Machine Learning as well as Finance.</p>
-        <p>I am passionate about leveraging technology for societal needs as well as using technology for fun experiences such as digital birthday cards for long distance friends and family!</p>
-        <p>Previously, I was an Innovation Developer Intern at Blue Shield of California where I contributed to the automation of prior authorization using Da Vinci and the development of an online consolidation of member health data called Member Health Record (MHR).</p>
-        <p>When I am not developing, I love watching older movies and TV shows (so nostalgic!), making Spotify playlists for friends, karaoking, eating lots of yummy food, and indulging in my, very long, TBR list! Let's connect!</p>
+      <main className={styles.container}>
+        <h1 className={styles.header}>Hi, I'm Jessie!</h1>
+        <p className={styles.paragraph}>I am an undergraduate student at Princeton University studying Computer Science and minoring in Statistics and Machine Learning as well as Finance.</p>
+        <p className={styles.paragraph}>I am passionate about leveraging technology for societal needs as well as using technology for fun experiences such as digital birthday cards for long distance friends and family!</p>
+        <p className={styles.paragraph}>Previously, I was an Innovation Developer Intern at Blue Shield of California where I contributed to the automation of prior authorization using Da Vinci and the development of an online consolidation of member health data called Member Health Record (MHR).</p>
+        <p className={styles.paragraph}>When I am not developing, I love watching older movies and TV shows (so nostalgic!), making Spotify playlists for friends, karaoke-ing, eating lots of yummy food, and indulging in my, very long, TBR list! Let's connect!</p>
         <a href="/resume" className="btn">View Resume</a>
       </main>
     </>
